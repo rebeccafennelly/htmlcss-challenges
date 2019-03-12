553d64331e0b83ec79938d08b5936d33a14d3496
Shea Murphy <Shea.Murphy@nology.io>
2019-03-12 14:42:51 +0000

    Created logical sections for Jquery funcs

--- frameworks/jquery/main.js
+++ frameworks/jquery/main.js
@@ -1,31 +1,30 @@
-$(".content").hide();
+$("section").hide();
 $("#fade-div").hide();
-$("#slide-div").hide();
 
-$("#show").click(function() {
-  $(".content").show();
+$("h1").click(function() {
+  $("section").show();
 });
 
-function hideContent() {
-  $(".content").hide();
-}
+$("#section-hide").click(function() {
+  $("section").hide();
+});
 
 function editText(input) {
   $("#output").text(input);
 }
 
-$("#hover-div").mouseenter(function() {
-  // console.log("hello");
-  $("#fade-div").fadeIn(2000);
-  $("#slide-div").slideDown("slow");
+$("#color-change").click(function() {
+  $("#output").css("color", "red");
 });
 
-$("#animator").click(function() {
+$("#fade-btn").click(function() {
+  $("#fade-div").fadeIn(2000);
+});
+
+$("#animate-btn").click(function() {
   $("#fade-div").animate({
-    height: "200px",
-    left: "50px"
+    left: "10px",
+    height: "150px",
+    width: "150px"
   });
-  $("#hover-prompt").hide();
-  $("#hover-div").css("background-color", "#0094e4");
-  $("main").css("background-image", "url('./background-2.jpg')");
 });
